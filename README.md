e4644f142b166405332f2f3b37ef597ceb01d358
Jeff Trotman <jeff@jefftrotman.com>
2020-02-23 06:47:14 -0600

    Kendo functionality

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ It uses a Migration to create the database, create the table and populate the ta
 ## 2. Scaffolded Cities controller and views
 
 A new controller was added (Cities) and used the "MVC 5 Controller with views, using Entity Framework" option. The only other change was the Cities was added to the top level menu in the \_Layout.cshtml in the Views\Shared folder.
+
+## 3. Adding Telerik Kendo MVC UI 
+
+A new controller was added (KendoCity) using the Kendo UI Scaffolder option. This demonstrates the Kendo grid with editing functionality.
+This requires that you add a custom NuGet feed that Telerik provides as described here - https://docs.telerik.com/aspnet-mvc/getting-started/installation/nuget-install.
+You should install Telerik.UI.for.AspNet.Mvc5.Trial, version 2020.1.114.
---
+++ Technicality.AspNetTrainingOne/Controllers/KendoCityController.cs
@@ -0,0 +1,117 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Technicality.AspNetTrainingOne;
+
+namespace Technicality.AspNetTrainingOne.Controllers
+{
+    public class KendoCityController : Controller
+    {
+        private TrainingOneContext db = new TrainingOneContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cities_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<City> cities = db.Cities;
+            DataSourceResult result = cities.ToDataSourceResult(request, city => new {
+                CityId = city.CityId,
+                CityName = city.CityName,
+                State = city.State
+            });
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Cities_Create([DataSourceRequest]DataSourceRequest request, City city)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new City
+                {
+                    CityName = city.CityName,
+                    State = city.State
+                };
+
+                db.Cities.Add(entity);
+                db.SaveChanges();
+                city.CityId = entity.CityId;
+            }
+
+            return Json(new[] { city }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Cities_Update([DataSourceRequest]DataSourceRequest request, City city)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new City
+                {
+                    CityId = city.CityId,
+                    CityName = city.CityName,
+                    State = city.State
+                };
+
+                db.Cities.Attach(entity);
+                db.Entry(entity).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Json(new[] { city }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Cities_Destroy([DataSourceRequest]DataSourceRequest request, City city)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new City
+                {
+                    CityId = city.CityId,
+                    CityName = city.CityName,
+                    State = city.State
+                };
+
+                db.Cities.Attach(entity);
+                db.Cities.Remove(entity);
+                db.SaveChanges();
+            }
+
+            return Json(new[] { city }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Excel_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+    
+        [HttpPost]
+        public ActionResult Pdf_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
